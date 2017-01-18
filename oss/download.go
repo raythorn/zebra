188e937cda02022a528f730fd052da32483ad090
Derek Ray <derek.ray@raythorn.com>
2017-01-18 16:28:21 +0800

    modify downlowd

--- oss/download.go
+++ oss/download.go
@@ -1,10 +1,23 @@
 package oss
 
 import (
-	"fmt"
 	"github.com/raythorn/falcon/context"
+	"os"
+	"path"
 )
 
 func Download(ctx *context.Context) {
-	fmt.Println("Download")
+	// root := ctx.Get(OssRootKey)
+	// fp := ctx.Get(OssPathKey)
+
+	// if root == "" {
+	// 	root, _ = os.Getwd()
+	// }
+
+	// fp = path.Clean(root + "/" + fp)
+
+	// fi, err := os.Stat(fp)
+	// if err != nil || !os.IsExist(err) {
+
+	// }
 }
